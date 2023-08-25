cdf3278b7ecde52357d04199731b2da0f21b0fe5
csanbar <csanbar@gmail.com>
2023-08-25 09:25:24 +0200

    add uploads folder & some images

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import cors from 'cors'
 const app = express()
 
 // MIDDLEWARES
-
+app.use('/uploads', express.static('uploads'))
 app.use(express.json())
 app.use(cors())
 app.use((req, res, next) => {
