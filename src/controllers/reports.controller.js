e59ff76d0225acbb3165770ba02f74f8def6a8fa
csanbar <csanbar@gmail.com>
2023-09-13 16:43:42 +0200

    add feature to prevent duplicated reports

--- src/controllers/reports.controller.js
+++ src/controllers/reports.controller.js
@@ -13,6 +13,9 @@ export const createReport = async (req, res) => {
   try {
     const { id } = req.params
 
+    if(await alreadyReported(id, req.id_user)){
+      return res.status(401).json({})
+    }
 
     const [rows] = await pool.query('insert into reports (id_tutorial, id_user) values(?, ?)', [id, req.id_user])
 
@@ -23,4 +26,23 @@ export const createReport = async (req, res) => {
       error: error.toString()
     })
   }
+}
+
+/**
+ * Checks if a tutorial was already reported by the selected user
+ * 
+ * 
+ * @param {*} id_tutorial 
+ * @param {*} id_user 
+ * @returns true or false
+ */
+async function alreadyReported(id_tutorial, id_user){
+
+  const [rows] = await pool.query("select * from reports where id_tutorial = ? and id_user = ?", [id_tutorial, id_user])
+
+  if(rows.length <= 0){
+   return false
+  }  
+
+  return true
 }
